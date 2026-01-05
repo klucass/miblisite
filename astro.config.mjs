d64fed1e3037af542d9cd62cd31ba63d05531116
Kennedy Lucas <kennedy.lucas@gmail.com>
2026-01-05 16:47:41 -0300

    change config for performance enhancement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
   vite: {
-      plugins: [tailwindcss()            
-      ]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [sitemap()]
+  integrations: [sitemap()],
 });
